00de1a6166de855bd43356ab994acb46f5f89f47
Mateus Abelli <mateusabelli@gmail.com>
2023-09-10 18:36:23 -0300

    Implement getList()

--- src/provider.ts
+++ src/provider.ts
@@ -2,200 +2,235 @@ import { AxiosInstance } from "axios";
 import { stringify } from "query-string";
 import { DataProvider } from "@refinedev/core";
 import { axiosInstance, generateSort, generateFilter } from "./utils";
+import sqlite3 from "sqlite3";
 
 type MethodTypes = "get" | "delete" | "head" | "options";
 type MethodTypesWithBody = "post" | "put" | "patch";
 
 export const dataProvider = (
     apiUrl: string,
-    httpClient: AxiosInstance = axiosInstance,
+    // httpClient: AxiosInstance = axiosInstance,
 ): Omit<
     Required<DataProvider>,
-    "createMany" | "updateMany" | "deleteMany"
+    "createMany" |
+    "updateMany" |
+    "deleteMany" |
+    "custom" |
+    "getApiUrl" |
+    "deleteOne" |
+    "getOne" |
+    "create" |
+    "update" |
+    "getMany"
+
 > => ({
     getList: async ({ resource, pagination, filters, sorters, meta }) => {
-        const url = `${apiUrl}/${resource}`;
+        try {
+            const db = new sqlite3.Database(apiUrl);
+            // const url = `${apiUrl}/${resource}`;
 
-        const {
-            current = 1,
-            pageSize = 10,
-            mode = "server",
-        } = pagination ?? {};
+            // const {
+            //     current = 1,
+            //     pageSize = 10,
+            //     mode = "server",
+            // } = pagination ?? {};
+            //
+            // const { headers: headersFromMeta, method } = meta ?? {};
+            // const requestMethod = (method as MethodTypes) ?? "get";
+            //
+            // const queryFilters = generateFilter(filters);
+            //
+            // const query: {
+            //     _start?: number;
+            //     _end?: number;
+            //     _sort?: string;
+            //     _order?: string;
+            // } = {};
+            //
+            // if (mode === "server") {
+            //     query._start = (current - 1) * pageSize;
+            //     query._end = current * pageSize;
+            // }
+            //
+            // const generatedSort = generateSort(sorters);
+            // if (generatedSort) {
+            //     const { _sort, _order } = generatedSort;
+            //     query._sort = _sort.join(",");
+            //     query._order = _order.join(",");
+            // }
 
-        const { headers: headersFromMeta, method } = meta ?? {};
-        const requestMethod = (method as MethodTypes) ?? "get";
+            // const { data, headers } = await httpClient[requestMethod](
+            //     `${url}?${stringify(query)}&${stringify(queryFilters)}`,
+            //     {
+            //         headers: headersF1romMeta,
+            //     },
+            // );
+            //
+            // const total = +headers["x-total-count"];
 
-        const queryFilters = generateFilter(filters);
+            const rows = await new Promise((resolve, reject) => {
+                db.all(`SELECT * FROM ${resource}`, (err, rows) => {
+                    if (err) {
+                        reject(err)
+                    } else {
+                        resolve(rows);
+                    }
+                    db.close();
+                }
+                );
+            }) as Array<any>
 
-        const query: {
-            _start?: number;
-            _end?: number;
-            _sort?: string;
-            _order?: string;
-        } = {};
+            let data = rows;
+            let total = rows?.length ?? 0;
 
-        if (mode === "server") {
-            query._start = (current - 1) * pageSize;
-            query._end = current * pageSize;
-        }
-
-        const generatedSort = generateSort(sorters);
-        if (generatedSort) {
-            const { _sort, _order } = generatedSort;
-            query._sort = _sort.join(",");
-            query._order = _order.join(",");
-        }
-
-        const { data, headers } = await httpClient[requestMethod](
-            `${url}?${stringify(query)}&${stringify(queryFilters)}`,
-            {
-                headers: headersFromMeta,
-            },
-        );
-
-        const total = +headers["x-total-count"];
-
-        return {
-            data,
-            total: total || data.length,
-        };
-    },
-
-    getMany: async ({ resource, ids, meta }) => {
-        const { headers, method } = meta ?? {};
-        const requestMethod = (method as MethodTypes) ?? "get";
-
-        const { data } = await httpClient[requestMethod](
-            `${apiUrl}/${resource}?${stringify({ id: ids })}`,
-            { headers },
-        );
-
-        return {
-            data,
-        };
-    },
-
-    create: async ({ resource, variables, meta }) => {
-        const url = `${apiUrl}/${resource}`;
-
-        const { headers, method } = meta ?? {};
-        const requestMethod = (method as MethodTypesWithBody) ?? "post";
-
-        const { data } = await httpClient[requestMethod](url, variables, {
-            headers,
-        });
-
-        return {
-            data,
-        };
-    },
-
-    update: async ({ resource, id, variables, meta }) => {
-        const url = `${apiUrl}/${resource}/${id}`;
-
-        const { headers, method } = meta ?? {};
-        const requestMethod = (method as MethodTypesWithBody) ?? "patch";
-
-        const { data } = await httpClient[requestMethod](url, variables, {
-            headers,
-        });
-
-        return {
-            data,
-        };
-    },
-
-    getOne: async ({ resource, id, meta }) => {
-        const url = `${apiUrl}/${resource}/${id}`;
-
-        const { headers, method } = meta ?? {};
-        const requestMethod = (method as MethodTypes) ?? "get";
-
-        const { data } = await httpClient[requestMethod](url, { headers });
-
-        return {
-            data,
-        };
-    },
-
-    deleteOne: async ({ resource, id, variables, meta }) => {
-        const url = `${apiUrl}/${resource}/${id}`;
-
-        const { headers, method } = meta ?? {};
-        const requestMethod = (method as MethodTypesWithBody) ?? "delete";
-
-        const { data } = await httpClient[requestMethod](url, {
-            data: variables,
-            headers,
-        });
-
-        return {
-            data,
-        };
-    },
-
-    getApiUrl: () => {
-        return apiUrl;
-    },
-
-    custom: async ({
-        url,
-        method,
-        filters,
-        sorters,
-        payload,
-        query,
-        headers,
-    }) => {
-        let requestUrl = `${url}?`;
-
-        if (sorters) {
-            const generatedSort = generateSort(sorters);
-            if (generatedSort) {
-                const { _sort, _order } = generatedSort;
-                const sortQuery = {
-                    _sort: _sort.join(","),
-                    _order: _order.join(","),
-                };
-                requestUrl = `${requestUrl}&${stringify(sortQuery)}`;
+            return {
+                data,
+                total: total || data.length,
+            };
+        } catch (err) {
+            console.error("Error in getList()", err);
+            return {
+                data: [],
+                total: 0,
             }
         }
-
-        if (filters) {
-            const filterQuery = generateFilter(filters);
-            requestUrl = `${requestUrl}&${stringify(filterQuery)}`;
-        }
-
-        if (query) {
-            requestUrl = `${requestUrl}&${stringify(query)}`;
-        }
-
-        if (headers) {
-            httpClient.defaults.headers = {
-                ...httpClient.defaults.headers,
-                ...headers,
-            };
-        }
-
-        let axiosResponse;
-        switch (method) {
-            case "put":
-            case "post":
-            case "patch":
-                axiosResponse = await httpClient[method](url, payload);
-                break;
-            case "delete":
-                axiosResponse = await httpClient.delete(url, {
-                    data: payload,
-                });
-                break;
-            default:
-                axiosResponse = await httpClient.get(requestUrl);
-                break;
-        }
-
-        const { data } = axiosResponse;
-
-        return Promise.resolve({ data });
     },
+
+    // getMany: async ({ resource, ids, meta }) => {
+    //     const { headers, method } = meta ?? {};
+    //     const requestMethod = (method as MethodTypes) ?? "get";
+    //
+    //     const { data } = await httpClient[requestMethod](
+    //         `${apiUrl}/${resource}?${stringify({ id: ids })}`,
+    //         { headers },
+    //     );
+    //
+    //     return {
+    //         data,
+    //     };
+    // },
+
+    // create: async ({ resource, variables, meta }) => {
+    //     const url = `${apiUrl}/${resource}`;
+    //
+    //     const { headers, method } = meta ?? {};
+    //     const requestMethod = (method as MethodTypesWithBody) ?? "post";
+    //
+    //     const { data } = await httpClient[requestMethod](url, variables, {
+    //         headers,
+    //     });
+    //
+    //     return {
+    //         data,
+    //     };
+    // },
+
+    // update: async ({ resource, id, variables, meta }) => {
+    //     const url = `${apiUrl}/${resource}/${id}`;
+    //
+    //     const { headers, method } = meta ?? {};
+    //     const requestMethod = (method as MethodTypesWithBody) ?? "patch";
+    //
+    //     const { data } = await httpClient[requestMethod](url, variables, {
+    //         headers,
+    //     });
+    //
+    //     return {
+    //         data,
+    //     };
+    // },
+
+    // getOne: async ({ resource, id, meta }) => {
+    //     const url = `${apiUrl}/${resource}/${id}`;
+    //
+    //     const { headers, method } = meta ?? {};
+    //     const requestMethod = (method as MethodTypes) ?? "get";
+    //
+    //     const { data } = await httpClient[requestMethod](url, { headers });
+    //
+    //     return {
+    //         data,
+    //     };
+    // },
+    //
+    // deleteOne: async ({ resource, id, variables, meta }) => {
+    //     const url = `${apiUrl}/${resource}/${id}`;
+    //
+    //     const { headers, method } = meta ?? {};
+    //     const requestMethod = (method as MethodTypesWithBody) ?? "delete";
+    //
+    //     const { data } = await httpClient[requestMethod](url, {
+    //         data: variables,
+    //         headers,
+    //     });
+    //
+    //     return {
+    //         data,
+    //     };
+    // },
+
+    // getApiUrl: () => {
+    //     return apiUrl;
+    // },
+
+    // custom: async ({
+    //     url,
+    //     method,
+    //     filters,
+    //     sorters,
+    //     payload,
+    //     query,
+    //     headers,
+    // }) => {
+    //     let requestUrl = `${url}?`;
+    //
+    //     if (sorters) {
+    //         const generatedSort = generateSort(sorters);
+    //         if (generatedSort) {
+    //             const { _sort, _order } = generatedSort;
+    //             const sortQuery = {
+    //                 _sort: _sort.join(","),
+    //                 _order: _order.join(","),
+    //             };
+    //             requestUrl = `${requestUrl}&${stringify(sortQuery)}`;
+    //         }
+    //     }
+    //
+    //     if (filters) {
+    //         const filterQuery = generateFilter(filters);
+    //         requestUrl = `${requestUrl}&${stringify(filterQuery)}`;
+    //     }
+    //
+    //     if (query) {
+    //         requestUrl = `${requestUrl}&${stringify(query)}`;
+    //     }
+    //
+    //     if (headers) {
+    //         httpClient.defaults.headers = {
+    //             ...httpClient.defaults.headers,
+    //             ...headers,
+    //         };
+    //     }
+    //
+    //     let axiosResponse;
+    //     switch (method) {
+    //         case "put":
+    //         case "post":
+    //         case "patch":
+    //             axiosResponse = await httpClient[method](url, payload);
+    //             break;
+    //         case "delete":
+    //             axiosResponse = await httpClient.delete(url, {
+    //                 data: payload,
+    //             });
+    //             break;
+    //         default:
+    //             axiosResponse = await httpClient.get(requestUrl);
+    //             break;
+    //     }
+    //
+    //     const { data } = axiosResponse;
+    //
+    //     return Promise.resolve({ data });
+    // },
 });
--- test/getList/index.spec.ts
+++ test/getList/index.spec.ts
@@ -1,86 +1,101 @@
-import axios from "axios";
+// import axios from "axios";
 
-import JsonServer from "../../src/index";
-import "./index.mock";
+// import JsonServer from "../../src/index";
+import dataProvider from "../../src/index";
+// import "./index.mock";
 
-axios.defaults.adapter = require("axios/lib/adapters/http");
+// axios.defaults.adapter = require("axios/lib/adapters/http");
 
 describe("getList", () => {
     it("correct response", async () => {
-        const response = await JsonServer(
-            "https://api.fake-rest.refine.dev",
-            axios,
-        ).getList({ resource: "posts" });
+        const response = await dataProvider("./database.db")
+            .getList({ resource: "posts" });
 
         expect(response.data[0]["id"]).toBe(1);
-        expect(response.data[0]["title"]).toBe(
-            "Mollitia ipsam nisi in porro velit asperiores et quaerat dolorem.",
-        );
-        expect(response.total).toBe(1000);
-    });
+        expect(response.data[0]["text"]).toBe("Hello World");
+        expect(response.total).toBe(8);
 
-    it("correct sorting response", async () => {
-        const response = await JsonServer(
-            "https://api.fake-rest.refine.dev",
-            axios,
-        ).getList({
-            resource: "posts",
-            sorters: [
-                {
-                    field: "id",
-                    order: "asc",
-                },
-            ],
-        });
-
-        expect(response.data[0]["id"]).toBe(1);
-        expect(response.data[0]["title"]).toBe(
-            "Mollitia ipsam nisi in porro velit asperiores et quaerat dolorem.",
-        );
-        expect(response.total).toBe(1000);
-    });
-
-    it("correct filter response", async () => {
-        const response = await JsonServer(
-            "https://api.fake-rest.refine.dev",
-            axios,
-        ).getList({
-            resource: "posts",
-            filters: [
-                {
-                    field: "category.id",
-                    operator: "eq",
-                    value: ["1"],
-                },
-            ],
-        });
-
-        expect(response.data[0]["category"]["id"]).toBe(1);
-        expect(response.total).toBe(17);
-    });
-
-    it("correct filter and sort response", async () => {
-        const response = await JsonServer(
-            "https://api.fake-rest.refine.dev",
-            axios,
-        ).getList({
-            resource: "posts",
-            filters: [
-                {
-                    field: "category.id",
-                    operator: "eq",
-                    value: ["1"],
-                },
-            ],
-            sorters: [
-                {
-                    field: "id",
-                    order: "asc",
-                },
-            ],
-        });
-
-        expect(response.data[0]["id"]).toBe(44);
-        expect(response.total).toBe(17);
-    });
+        console.debug("data[0]: ", response.data[0]);
+        console.debug("total: ", response.total);
+    })
 });
+
+// describe("getList", () => {
+//     it("correct response", async () => {
+//         const response = await JsonServer(
+//             "https://api.fake-rest.refine.dev",
+//             axios,
+//         ).getList({ resource: "posts" });
+//
+//         expect(response.data[0]["id"]).toBe(1);
+//         expect(response.data[0]["title"]).toBe(
+//             "Mollitia ipsam nisi in porro velit asperiores et quaerat dolorem.",
+//         );
+//         expect(response.total).toBe(1000);
+//     });
+//
+//     it("correct sorting response", async () => {
+//         const response = await JsonServer(
+//             "https://api.fake-rest.refine.dev",
+//             axios,
+//         ).getList({
+//             resource: "posts",
+//             sorters: [
+//                 {
+//                     field: "id",
+//                     order: "asc",
+//                 },
+//             ],
+//         });
+//
+//         expect(response.data[0]["id"]).toBe(1);
+//         expect(response.data[0]["title"]).toBe(
+//             "Mollitia ipsam nisi in porro velit asperiores et quaerat dolorem.",
+//         );
+//         expect(response.total).toBe(1000);
+//     });
+//
+//     it("correct filter response", async () => {
+//         const response = await JsonServer(
+//             "https://api.fake-rest.refine.dev",
+//             axios,
+//         ).getList({
+//             resource: "posts",
+//             filters: [
+//                 {
+//                     field: "category.id",
+//                     operator: "eq",
+//                     value: ["1"],
+//                 },
+//             ],
+//         });
+//
+//         expect(response.data[0]["category"]["id"]).toBe(1);
+//         expect(response.total).toBe(17);
+//     });
+//
+//     it("correct filter and sort response", async () => {
+//         const response = await JsonServer(
+//             "https://api.fake-rest.refine.dev",
+//             axios,
+//         ).getList({
+//             resource: "posts",
+//             filters: [
+//                 {
+//                     field: "category.id",
+//                     operator: "eq",
+//                     value: ["1"],
+//                 },
+//             ],
+//             sorters: [
+//                 {
+//                     field: "id",
+//                     order: "asc",
+//                 },
+//             ],
+//         });
+//
+//         expect(response.data[0]["id"]).toBe(44);
+//         expect(response.total).toBe(17);
+//     });
+// });
