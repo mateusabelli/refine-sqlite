89c7207784d7c860fef8f908052ca01d848e958d
Mateus Abelli <mateusabelli@gmail.com>
2023-09-22 20:54:42 -0300

    Update getList test

--- test/getList/index.spec.ts
+++ test/getList/index.spec.ts
@@ -1,10 +1,4 @@
-// import axios from "axios";
-
-// import JsonServer from "../../src/index";
 import dataProvider from "../../src/index";
-// import "./index.mock";
-
-// axios.defaults.adapter = require("axios/lib/adapters/http");
 
 describe("getList", () => {
     const apiUrl = "./test.db"
@@ -35,7 +29,6 @@ describe("getList", () => {
         expect(response.total).toBe(6);
     });
 
-    // TODO: fix this test
     it("correct filter response", async () => {
         const response = await dataProvider(apiUrl)
             .getList({
@@ -53,7 +46,6 @@ describe("getList", () => {
         expect(response.total).toBe(2);
     });
 
-    // TODO: fix this test
     it("correct filter and sort response", async () => {
         const response = await dataProvider(apiUrl)
             .getList({
@@ -67,93 +59,13 @@ describe("getList", () => {
                 ],
                 sorters: [
                     {
-                        field: "id",
+                        field: "title",
                         order: "asc",
                     },
                 ],
             });
 
-        expect(response.data[0]["id"]).toBe(44);
-        expect(response.total).toBe(17);
+        expect(response.data[0]["id"]).toBe(6);
+        expect(response.total).toBe(2);
     });
 });
-
-// describe("getList", () => {
-//     it("correct response", async () => {
-//         const response = await JsonServer(
-//             "https://api.fake-rest.refine.dev",
-//             axios,
-//         ).getList({ resource: "posts" });
-//
-//         expect(response.data[0]["id"]).toBe(1);
-//         expect(response.data[0]["title"]).toBe(
-//             "Mollitia ipsam nisi in porro velit asperiores et quaerat dolorem.",
-//         );
-//         expect(response.total).toBe(1000);
-//     });
-//
-//     it("correct sorting response", async () => {
-//         const response = await JsonServer(
-//             "https://api.fake-rest.refine.dev",
-//             axios,
-//         ).getList({
-//             resource: "posts",
-//             sorters: [
-//                 {
-//                     field: "id",
-//                     order: "asc",
-//                 },
-//             ],
-//         });
-//
-//         expect(response.data[0]["id"]).toBe(1);
-//         expect(response.data[0]["title"]).toBe(
-//             "Mollitia ipsam nisi in porro velit asperiores et quaerat dolorem.",
-//         );
-//         expect(response.total).toBe(1000);
-//     });
-//
-//     it("correct filter response", async () => {
-//         const response = await JsonServer(
-//             "https://api.fake-rest.refine.dev",
-//             axios,
-//         ).getList({
-//             resource: "posts",
-//             filters: [
-//                 {
-//                     field: "category.id",
-//                     operator: "eq",
-//                     value: ["1"],
-//                 },
-//             ],
-//         });
-//
-//         expect(response.data[0]["category"]["id"]).toBe(1);
-//         expect(response.total).toBe(17);
-//     });
-//
-//     it("correct filter and sort response", async () => {
-//         const response = await JsonServer(
-//             "https://api.fake-rest.refine.dev",
-//             axios,
-//         ).getList({
-//             resource: "posts",
-//             filters: [
-//                 {
-//                     field: "category.id",
-//                     operator: "eq",
-//                     value: ["1"],
-//                 },
-//             ],
-//             sorters: [
-//                 {
-//                     field: "id",
-//                     order: "asc",
-//                 },
-//             ],
-//         });
-//
-//         expect(response.data[0]["id"]).toBe(44);
-//         expect(response.total).toBe(17);
-//     });
-// });
