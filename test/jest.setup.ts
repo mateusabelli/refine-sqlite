4ab04b934046bba1d230aaaa5eb537b76a427ed8
Mateus Abelli <mateusabelli@gmail.com>
2023-10-14 22:30:29 -0300

    Fix beforeAll DB deletion and recreation

--- test/jest.setup.ts
+++ test/jest.setup.ts
@@ -2,31 +2,14 @@ import sqlite3 from "sqlite3";
 import fs from "fs";
 
 beforeAll(async () => {
+    const dbPath = "test.db";
     try {
-        // Connect to the database
-        const db = new sqlite3.Database("./test.db");
-
-        // Clean the database
-        const dbTables = await new Promise((resolve, reject) => {
-            const sql = "SELECT name FROM sqlite_master WHERE type='table';"
-
-            db.all(sql, (err, rows) => {
-                if (err) {
-                    reject(err);
-                }
-                resolve(rows);
-            });
-        }) as Array<any>
-
-        dbTables.forEach((table) => {
-            const sql = `DROP TABLE ${table["name"]}`;
-
-            db.run(sql, (err) => {
-                if (err) {
-                    throw new Error("Error in database clean up" + err);
-                }
-            });
-        });
+        // Delete the database if it exists (DB must be disconnected first)
+        if (fs.existsSync(dbPath)) {
+            fs.rmSync(dbPath);
+        }
+        // Create the database
+        const db = new sqlite3.Database(dbPath);
 
         // Read the sql file
         const sql = fs.readFileSync("./test.sql").toString();
@@ -50,7 +33,7 @@ beforeAll(async () => {
             });
             db.close();
         });
-    } catch (error) {
+    } catch (error: any) {
         console.error("Error in beforeAll: ", error);
     }
 })
