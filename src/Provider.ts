54775eaa88408d88c2f119b0784e5dcaf20b13db
Mateus Abelli <mateusabelli@gmail.com>
2023-11-12 17:01:56 -0300

    Refactor and update types

--- src/Provider.ts
+++ src/Provider.ts
@@ -1,128 +1,140 @@
-import { CreateParams, UpdateParams } from "./interfaces/MethodParams";
-import Database from "./utils/Database";
+import { BaseRecord, DataProvider } from "@refinedev/core";
+import * as Types from "@refinedev/core/dist/contexts/data/IDataContext"
 
 import { Database as SQLiteDatabase } from "sql.js"
+import Database from "./utils/Database";
 
-class Provider {
-  private dbArgs: string
-  private type: 'local' | 'inMemory'
+class Provider implements DataProvider {
+    private databasePath: string
+    private databaseType: 'local' | 'inMemory'
 
-  constructor(type: 'local' | 'inMemory', dbArgs: string) {
-    this.dbArgs = dbArgs;
-    this.type = type;
-  }
-
-  private async dbConnect(): Promise<SQLiteDatabase | void> {
-    try {
-      return await Database.init(this.dbArgs);
-    } catch (error) {
-      console.error("Error in dbConnect()", error);
+    constructor(databaseType: 'local' | 'inMemory', databasePath: string) {
+        this.databasePath = databasePath;
+        this.databaseType = databaseType;
     }
-  }
 
-  public async create(params: CreateParams) {
-    const database = await this.dbConnect();
-    const { resource, variables } = params;
-
-    try {
-      if (!database)
-        throw new Error("Database connection not available.");
-
-      const columns = Object.keys(variables).join(", ")
-      const values = Object.values(variables).map((value) => `'${value}'`).join(", ")
-
-      let insertSql = `INSERT INTO ${resource} (${columns}) VALUES (${values})`;
-      let selectSql = `SELECT * FROM ${resource} WHERE (${columns}) = (${values})`;
-
-      database.run(insertSql);
-      const data = database.exec(selectSql)
-      const dataColumns = data[0].columns
-      const dataValues = data[0].values[0]
-
-      if (data) {
-        return {
-          data: Object.fromEntries(
-            dataColumns.map((col, index) => [col, dataValues[index]]),
-          )
+    private async dbConnect(): Promise<SQLiteDatabase | void> {
+        try {
+            return await Database.init(this.databaseType, this.databasePath);
+        } catch (error) {
+            console.error("Error in dbConnect()", error);
         }
-      } else {
-        throw new Error(`Could not create data in ${resource}`);
-      }
-    } catch (error) {
-      console.error("Error in create()", error);
-      return {
-        data: {}
-      }
-    } finally {
-      database?.close()
     }
-  }
 
-  public async update(params: UpdateParams) {
-    const database = await this.dbConnect();
-    const { resource, id, variables } = params;
+    public async create<TData extends BaseRecord = BaseRecord, TVariables = {}>
+        (params: Types.CreateParams<TVariables>): Promise<Types.CreateResponse<TData>> {
 
-    try {
-      if (!database)
-        throw new Error("Database connection not available.");
+        const database = await this.dbConnect();
+        const { resource, variables } = params;
 
-      const columns = Object.keys(variables || {});
-      const values = Object.values(variables || {});
+        try {
+            if (!database)
+                throw new Error("Database connection not available.");
 
-      let updateQuery = "";
+            if (!variables)
+                throw new Error("Variables were not provided.");
 
-      columns.forEach((column, index) => {
-        updateQuery += `${column} = '${values[index]}', `;
-      });
+            const columns = Object.keys(variables).join(", ")
+            const values = Object.values(variables).map((value) => `'${value}'`).join(", ")
 
-      // Slices the last comma
-      updateQuery = updateQuery.slice(0, -2);
+            let insertSql = `INSERT INTO ${resource} (${columns}) VALUES (${values})`;
+            let selectSql = `SELECT * FROM ${resource} WHERE (${columns}) = (${values})`;
 
-      const updateSql = `UPDATE ${resource} SET ${updateQuery} WHERE id = ${id}`;
-      const selectSql = `SELECT * FROM ${resource} WHERE id = ${id}`;
+            database.run(insertSql);
+            const data = database.exec(selectSql)
+            const dataColumns = data[0].columns
+            const dataValues = data[0].values[0]
 
-      database.run(updateSql);
-      const data = database.exec(selectSql)
-      const dataColumns = data[0].columns
-      const dataValues = data[0].values[0]
-
-      if (data) {
-        return {
-          data: Object.fromEntries(
-            dataColumns.map((col, index) => [col, dataValues[index]]),
-          )
+            if (data) {
+                return {
+                    data: Object.fromEntries(
+                        dataColumns.map((col, index) => [col, dataValues[index]]),
+                    ) as TData
+                }
+            } else {
+                throw new Error(`Could not create data in ${resource}`);
+            }
+        } catch (error) {
+            console.error("Error in create()", error);
+            return {
+                data: {} as TData
+            }
+        } finally {
+            database?.close()
         }
-      } else {
-        throw new Error(`Could not update data in ${resource}`);
-      }
-    } catch (error) {
-      console.error("Error in update()", error);
-      return {
-        data: {}
-      }
-    } finally {
-      database?.close()
     }
-  }
 
-  // deleteOne(params: DeleteOneParams) {
-  //     return DeleteOne.build(this.dbPath, params);
-  // }
-  //
-  // getOne(params: GetOneParams) {
-  //     return GetOne.build(this.dbPath, params);
-  // }
-  //
-  // getMany(params: GetManyParams) {
-  //     return GetMany.build(this.dbPath, params);
-  // }
-  //
-  // getList(params: GetListParams) {
-  //     return GetList.build(this.dbPath, params);
-  // }
+    public async update<TData extends BaseRecord = BaseRecord, TVariables = {}>
+        (params: Types.UpdateParams<TVariables>): Promise<Types.UpdateResponse<TData>> {
+
+        const database = await this.dbConnect();
+        const { resource, id, variables } = params;
+
+        try {
+            if (!database)
+                throw new Error("Database connection not available.");
+
+            const columns = Object.keys(variables || {});
+            const values = Object.values(variables || {});
+
+            let updateQuery = "";
+
+            columns.forEach((column, index) => {
+                updateQuery += `${column} = '${values[index]}', `;
+            });
+
+            // Slices the last comma
+            updateQuery = updateQuery.slice(0, -2);
+
+            const updateSql = `UPDATE ${resource} SET ${updateQuery} WHERE id = ${id}`;
+            const selectSql = `SELECT * FROM ${resource} WHERE id = ${id}`;
+
+            database.run(updateSql);
+            const data = database.exec(selectSql)
+            const dataColumns = data[0].columns
+            const dataValues = data[0].values[0]
+
+            if (data) {
+                return {
+                    data: Object.fromEntries(
+                        dataColumns.map((col, index) => [col, dataValues[index]]),
+                    ) as TData
+                }
+            } else {
+                throw new Error(`Could not update data in ${resource}`);
+            }
+        } catch (error) {
+            console.error("Error in update()", error);
+            return {
+                data: {} as TData
+            }
+        } finally {
+            database?.close()
+        }
+    }
+
+    getList!: <TData extends BaseRecord = BaseRecord>(params: Types.GetListParams) => Promise<Types.GetListResponse<TData>>;
+    getOne!: <TData extends BaseRecord = BaseRecord>(params: Types.GetOneParams) => Promise<Types.GetOneResponse<TData>>;
+    deleteOne!: <TData extends BaseRecord = BaseRecord, TVariables = {}>(params: Types.DeleteOneParams<TVariables>) => Promise<Types.DeleteOneResponse<TData>>;
+    getApiUrl!: () => string;
+
+    // deleteOne(params: DeleteOneParams) {
+    //     return DeleteOne.build(this.dbPath, params);
+    // }
+    //
+    // getOne(params: GetOneParams) {
+    //     return GetOne.build(this.dbPath, params);
+    // }
+    //
+    // getMany(params: GetManyParams) {
+    //     return GetMany.build(this.dbPath, params);
+    // }
+    //
+    // getList(params: GetListParams) {
+    //     return GetList.build(this.dbPath, params);
+    // }
 }
-
-export const dataProvider = (type: 'local' | 'inMemory', dbArgs: string) => {
-  return new Provider(type, dbArgs);
+export const dataProvider = (databaseType: 'local' | 'inMemory', databasePath: string) => {
+    return new Provider(databaseType, databasePath);
 }
 
--- src/utils/Database.ts
+++ src/utils/Database.ts
@@ -1,16 +1,32 @@
-import initSqlJs, {Database as SQLiteDatabase} from "sql.js"
+import initSqlJs from "sql.js"
 import fetch from "cross-fetch"
 
 class Database {
+    private static SQL: initSqlJs.SqlJsStatic
+
     private constructor() { }
 
-    public static async init(dbPath: string): Promise<SQLiteDatabase> {
-        const SQL = await initSqlJs({
+    public static async init(databaseType: "local" | "inMemory", databasePath: string): Promise<initSqlJs.Database> {
+        this.SQL = await initSqlJs({
             locateFile: () => "node_modules/sql.js/dist/sql-wasm.wasm"
         });
 
-        const dbFile = await fetch(dbPath).then(res => res.arrayBuffer());
-        return new SQL.Database(new Uint8Array(dbFile))
+        if (databaseType === "inMemory") {
+            return await this.inMemoryDatabase(databasePath);
+        }
+
+        return await this.localDatabase(databasePath);
+    }
+
+    private static async localDatabase(databasePath: string): Promise<initSqlJs.Database> {
+        const fs = require("fs");
+        const databaseFile = fs.readFileSync(databasePath);
+        return new this.SQL.Database(databaseFile);
+    }
+
+    private static async inMemoryDatabase(databasePath: string): Promise<initSqlJs.Database> {
+        const databaseFile = await fetch(databasePath).then(res => res.arrayBuffer());
+        return new this.SQL.Database(new Uint8Array(databaseFile))
     }
 }
 
