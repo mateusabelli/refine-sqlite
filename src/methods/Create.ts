014faf5590c9398c65516663e9595a222d23445b
Mateus Abelli <mateusabelli@gmail.com>
2023-11-12 16:59:36 -0300

    Comment unused code

--- src/methods/Create.ts
+++ src/methods/Create.ts
@@ -1,15 +1,15 @@
-import { CreateResponse } from "@refinedev/core";
-import { CreateParams } from "src/interfaces/MethodParams";
-import { promisify } from "util";
-import sqlite3 from "sqlite3";
-import Database from "../utils/Database";
-
-class Create {
-    private constructor() { }
-
-    static async build(params: CreateParams, dbPath?: string, ): Promise<CreateResponse> {
-
-    }
-}
-
-export default Create;
+// import { CreateResponse } from "@refinedev/core";
+// import { CreateParams } from "src/interfaces/MethodParams";
+// import { promisify } from "util";
+// import sqlite3 from "sqlite3";
+// import Database from "../utils/Database";
+//
+// class Create {
+//     private constructor() { }
+//
+//     static async build(params: CreateParams, dbPath?: string, ): Promise<CreateResponse> {
+//
+//     }
+// }
+//
+// export default Create;
--- src/methods/Update.ts
+++ src/methods/Update.ts
@@ -1,20 +1,20 @@
-import { UpdateResponse } from "@refinedev/core";
-import { UpdateParams } from "src/interfaces/MethodParams";
-import { promisify } from "util";
-import sqlite3 from "sqlite3";
-import Database from "../utils/Database";
-
-class Update {
-    private static dbInstance: Database;
-    private db: sqlite3.Database | null = null;
-
-    private constructor(dbPath: string) {
-        Update.dbInstance = Database.getInstance(dbPath);
-        this.db = Update.dbInstance.getDatabase();
-    }
-
-    static async build(dbPath: string, params: UpdateParams): Promise<UpdateResponse> {
-    }
-}
-
-export default Update;
+// import { UpdateResponse } from "@refinedev/core";
+// import { UpdateParams } from "src/interfaces/MethodParams";
+// import { promisify } from "util";
+// import sqlite3 from "sqlite3";
+// import Database from "../utils/Database";
+//
+// class Update {
+//     private static dbInstance: Database;
+//     private db: sqlite3.Database | null = null;
+//
+//     private constructor(dbPath: string) {
+//         Update.dbInstance = Database.getInstance(dbPath);
+//         this.db = Update.dbInstance.getDatabase();
+//     }
+//
+//     static async build(dbPath: string, params: UpdateParams): Promise<UpdateResponse> {
+//     }
+// }
+//
+// export default Update;
