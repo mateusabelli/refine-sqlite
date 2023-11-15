844efb793938be9f4ee1fd72466b5c60deaa6920
Mateus Abelli <mateusabelli@gmail.com>
2023-11-15 19:26:43 -0300

    Break everything to fix db state with methods

--- src/Provider.ts
+++ src/Provider.ts
@@ -1,31 +1,90 @@
 import { BaseRecord, DataProvider } from "@refinedev/core";
 import * as Types from "@refinedev/core/dist/contexts/data/IDataContext"
 
+import fetch from "cross-fetch";
 import { generateFilter, generateSort } from "./utils";
 import { Database as SQLiteDatabase } from "sql.js"
 import Database from "./utils/Database";
+import initSqlJs from "sql.js";
 
-class Provider implements DataProvider {
-    private databasePath: string
-    private databaseType?: undefined | 'inMemory'
+let databaseInstance: { initialized: boolean } | null = null;
+let databaseString: string | null = null;
 
-    constructor(databasePath: string, databaseType?: undefined | 'inMemory') {
-        this.databasePath = databasePath;
-        this.databaseType = databaseType;
+function toBinString(uarr: Uint8Array): string {
+    const strings: string[] = [];
+    const chunkSize = 0xffff;
+
+    // There is a maximum stack size. We cannot call String.fromCharCode with as many arguments as we want
+    for (let i = 0; i * chunkSize < uarr.length; i++) {
+        strings.push(String.fromCharCode.apply(null, Array.from(uarr.subarray(i * chunkSize, (i + 1) * chunkSize))));
     }
 
-    private async dbConnect(): Promise<SQLiteDatabase | void> {
+    return strings.join('');
+}
+
+function toBinArray(str: string): Uint8Array {
+    const l = str.length;
+    const arr = new Uint8Array(l);
+
+    for (let i = 0; i < l; i++) {
+        arr[i] = str.charCodeAt(i);
+    }
+
+    return arr;
+}
+
+async function dbConnect(databasePath: string) {
+    const SQL = await initSqlJs({
+        locateFile: () => "node_modules/sql.js/dist/sql-wasm.wasm"
+    });
+
+    if (!databaseInstance) {
         try {
-            return await Database.init(this.databasePath, this.databaseType);
+            const databaseFile = await fetch(databasePath)
+                .then(response => response.arrayBuffer())
+                .then(arrayBuffer => new Uint8Array(arrayBuffer));
+
+            databaseInstance = { initialized: true };
+            const database = new SQL.Database(databaseFile);
+            console.log("BEFORE", database.exec("SELECT * FROM posts")[0].values)
+
+            databaseString = toBinString(database.export())
+            console.log('Database initialized');
+
+            return database
         } catch (error) {
             console.error("Error in dbConnect()", error);
         }
     }
+    console.log('Database already initialized');
+    const database = new SQL.Database(toBinArray(databaseString!))
+    console.log("AFTER", database.exec("SELECT * FROM posts")[0].values)
+    return new SQL.Database(toBinArray(databaseString!))
+}
 
-    public async create<TData extends BaseRecord = BaseRecord, TVariables = {}>
-        (params: Types.CreateParams<TVariables>): Promise<Types.CreateResponse<TData>> {
-
-        const database = await this.dbConnect();
+export const dataProvider = (
+    databasePath: string,
+    databaseType?: undefined | 'inMemory'
+): Omit<
+    Required<DataProvider>,
+    "createMany" |
+    "updateMany" |
+    "deleteMany" |
+    "custom" |
+    "getMany" |
+    "getOne" |
+    "update" |
+    "deleteOne" |
+    "getApiUrl"
+> & { close: () => void; } => ({
+    close: () => {
+        databaseInstance = null;
+        databaseString = null;
+    },
+    create: async <TData extends BaseRecord = BaseRecord, TVariables = {}>(
+        params: Types.CreateParams<TVariables>,
+    ): Promise<Types.CreateResponse<TData>> => {
+        const database = await dbConnect(databasePath);
         const { resource, variables } = params;
 
         try {
@@ -61,169 +120,14 @@ class Provider implements DataProvider {
                 data: {} as TData
             }
         } finally {
+            databaseString = toBinString(database.export())
             database?.close()
         }
-    }
-
-    public async update<TData extends BaseRecord = BaseRecord, TVariables = {}>
-        (params: Types.UpdateParams<TVariables>): Promise<Types.UpdateResponse<TData>> {
-
-        const database = await this.dbConnect();
-        const { resource, id, variables } = params;
-
-        try {
-            if (!database)
-                throw new Error("Database connection not available.");
-
-            const columns = Object.keys(variables || {});
-            const values = Object.values(variables || {});
-
-            let updateQuery = "";
-
-            columns.forEach((column, index) => {
-                updateQuery += `${column} = '${values[index]}', `;
-            });
-
-            // Slices the last comma
-            updateQuery = updateQuery.slice(0, -2);
-
-            const updateSql = `UPDATE ${resource} SET ${updateQuery} WHERE id = ${id}`;
-            const selectSql = `SELECT * FROM ${resource} WHERE id = ${id}`;
-
-            database.run(updateSql);
-            const data = database.exec(selectSql)
-            const dataColumns = data[0].columns
-            const dataValues = data[0].values[0]
-
-            if (data) {
-                return {
-                    data: Object.fromEntries(
-                        dataColumns.map((column, index) => [column, dataValues[index]]),
-                    ) as TData
-                }
-            } else {
-                throw new Error(`Could not update data in ${resource}`);
-            }
-        } catch (error) {
-            console.error("Error in update()", error);
-            return {
-                data: {} as TData
-            }
-        } finally {
-            database?.close()
-        }
-    }
-
-
-    public async deleteOne<TData extends BaseRecord = BaseRecord, TVariables = {}>
-        (params: Types.DeleteOneParams<TVariables>): Promise<Types.DeleteOneResponse<TData>> {
-
-        const database = await this.dbConnect();
-        const { resource, id } = params;
-
-        try {
-            if (!database)
-                throw new Error("Database connection not available.");
-
-            let deleteSql = `DELETE FROM ${resource} WHERE id = ${id}`;
-            let selectSql = `SELECT * FROM ${resource} WHERE id = ${id}`;
-
-            database.run(deleteSql);
-            const data = database.exec(selectSql)
-
-            if (data.length > 0) {
-                throw new Error(`Could not delete data in ${resource} with id ${id}`);
-            } else {
-                return {
-                    data: {} as TData
-                }
-            }
-        } catch (error) {
-            console.error("Error in deleteOne()", error);
-            return {
-                data: {} as TData
-            }
-        } finally {
-            database?.close()
-        }
-    }
-
-    public async getOne<TData extends BaseRecord = BaseRecord>
-        (params: Types.GetOneParams): Promise<Types.GetOneResponse<TData>> {
-
-        const database = await this.dbConnect();
-        const { resource, id } = params;
-
-        try {
-            if (!database)
-                throw new Error("Database connection not available.");
-
-            const selectSql = `SELECT * FROM ${resource} WHERE id = ${id}`;
-
-            const data = database.exec(selectSql)
-            const dataColumns = data[0].columns
-            const dataValues = data[0].values[0]
-
-            if (data) {
-                return {
-                    data: Object.fromEntries(
-                        dataColumns.map((column, index) => [column, dataValues[index]]),
-                    ) as TData
-                }
-            } else {
-                throw new Error(`No data found in ${resource} and id ${id}`);
-            }
-        } catch (error) {
-            console.error("Error in getOne()", error);
-            return {
-                data: {} as TData
-            }
-        } finally {
-            database?.close()
-        }
-    }
-
-    public async getMany<TData extends BaseRecord = BaseRecord>
-        (params: Types.GetManyParams): Promise<Types.GetManyResponse<TData>> {
-
-        const database = await this.dbConnect();
-        const { resource, ids } = params;
-
-        try {
-            if (!database)
-                throw new Error("Database connection not available.");
-
-            let idString = ids.join(", ");
-
-            const selectSql = `SELECT * FROM ${resource} WHERE id IN (${idString})`;
-
-            const data = database.exec(selectSql)
-            const dataColumns = data[0].columns
-            const dataValues = data[0].values
-
-            if (data) {
-                return {
-                    data: dataValues.map((values) => (
-                        Object.fromEntries(values.map((value, index) => [dataColumns[index], value]))
-                    )) as TData[]
-                }
-            } else {
-                throw new Error(`No data found in ${resource} and ids ${idString}`);
-            }
-        } catch (error) {
-            console.error("Error in getMany()", error);
-            return {
-                data: []
-            }
-        } finally {
-            database?.close()
-        }
-    }
-
-    public async getList<TData extends BaseRecord = BaseRecord>
-        (params: Types.GetListParams): Promise<Types.GetListResponse<TData>> {
-
-        const database = await this.dbConnect();
+    },
+    getList: async <TData extends BaseRecord = BaseRecord>(
+        params: Types.GetListParams,
+    ): Promise<Types.GetListResponse<TData>> => {
+        const database = await dbConnect(databasePath);
         const { resource, pagination, filters, sorters } = params;
 
         try {
@@ -276,16 +180,8 @@ class Provider implements DataProvider {
                 total: 0
             }
         } finally {
+            databaseString = toBinString(database.export())
             database?.close()
         }
-    }
-
-    public getApiUrl(): string {
-        return this.databasePath
-    }
-}
-
-export const dataProvider = (databasePath: string, databaseType?: undefined | 'inMemory') => {
-    return new Provider(databasePath, databaseType);
-}
-
+    },
+})
